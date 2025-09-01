413482fdad0cd8e139af323be105e667bfe01fd2
Tejashwini Gundlapally <tejuaruna29@gmail.com>
2025-09-01 12:00:00 -0700

    Force dark mode by default

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Force dark mode
+document.documentElement.classList.add("dark");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
